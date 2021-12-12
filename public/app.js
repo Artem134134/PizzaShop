cfcc073a26fd0f3dd4e5352522cf317e6b6481da
Artem134134 <car322132@gmail.com>
2021-12-12 23:41:32 +0300

    Add Rakefile, Update Gemfile, add model

--- public/app.js
+++ public/app.js
@@ -1,4 +1,9 @@
-var x = 1;
 
-function something()
-{ alert(x);  x = x + 1; }
+function something() {
+  var x = window.localStorage.getItem('bbb');
+  x = x * 1 + 1;
+  window.localStorage.setItem('bbb', x);
+
+  alert(x);
+}
+
